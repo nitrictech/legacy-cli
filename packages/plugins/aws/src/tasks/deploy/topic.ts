d23f9e0b387a205a311871d9252e08ead40ac161
Jye Cusch <jye.cusch@gmail.com>
2020-12-21 09:15:55 +1100

    fix: incorrect topic property setting

--- packages/plugins/aws/src/tasks/deploy/topic.ts
+++ packages/plugins/aws/src/tasks/deploy/topic.ts
@@ -9,8 +9,16 @@ export default (topic: NitricTopic): Record<string, any> => {
 		[topicDefName]: {
 			Type: 'AWS::SNS::Topic',
 			Properties: {
-				DisplayName: topicName,
+				TopicName: topicName,
 			},
 		},
 	};
 };
+
+// TopicName
+// The name of the topic you want to create. Topic names must include only uppercase and lowercase ASCII letters, numbers, underscores, and hyphens, and must be between 1 and 256 characters long. FIFO topic names must end with .fifo.
+
+// If you don't specify a name, AWS CloudFormation generates a unique physical ID and uses that ID for the topic name. For more information, see Name Type.
+
+// Important
+// If you specify a name, you can't perform updates that require replacement of this resource. You can perform updates that require no or some interruption. If you must replace the resource, specify a new name.
