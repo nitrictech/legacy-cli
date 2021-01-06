d2636829cf28cd2e6de95ae0d26d8a4f83f465c1
Jye Cusch <jye.cusch@gmail.com>
2021-01-06 14:12:58 +1100

    test: initial make:project tests

--- packages/base/tests/make-project.test.ts
+++ packages/base/tests/make-project.test.ts
@@ -2,16 +2,33 @@ import 'jest';
 import fs from 'fs';
 import { MakeProjectTask } from '../src/tasks/make';
 
+jest.mock('fs');
+
 describe('Given executing nitric make:project', () => {
+	describe("When the project folder doesn't exist", () => {
+		test('The new project directory is created', async () => {
+			await new MakeProjectTask('test-project', true).do();
+			expect(fs.mkdirSync).toBeCalledWith('./test-project');
+		});
+	});
+
 	describe(`When the project folder exists`, () => {
 		jest.spyOn(fs, 'mkdirSync').mockImplementation(() => {
 			throw new Error('file already exists');
 		});
 
-		test('An error is returned when create not forced', () => {
-			expect(async () => {
-				await new MakeProjectTask('test-project', true).do();
-			}).rejects.toThrowError();
+		test('An error is returned when create not forced', async () => {
+			// Set 'force' parameter to false.
+			await expect(new MakeProjectTask('test-project', false).do()).rejects.toThrowError();
+		});
+
+		test('nitric.yaml is written when forced', async () => {
+			// Set 'force' parameter to true.
+			await expect(new MakeProjectTask('test-project', true).do()).resolves.toBe(undefined);
+			expect(fs.writeFileSync).toBeCalledWith(
+				'./test-project/nitric.yaml',
+				Buffer.from('name: test-project\n', 'utf-8'),
+			);
 		});
 	});
 });
