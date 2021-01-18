142597f3a77b4dfa382ab3a169e0e9a1f04da759
Tim <timothy.j.holm@gmail.com>
2021-01-19 09:24:34 +1100

    chore: Update utils tests.

--- packages/base/tests/utils/index.test.ts
+++ packages/base/tests/utils/index.test.ts
@@ -1,8 +1,10 @@
 import 'jest';
 // import * as os from 'os';
-import '../../src/utils';
-import { createNitricLogDir, getAvailableTemplates, isTemplateAvailable } from '../../src/utils';
+import * as utils from '../../src/utils';
+// import { createNitricLogDir, getAvailableTemplates, isTemplateAvailable } from '../../src/utils';
 import fs from 'fs';
+import { NitricTemplateRepository } from '../../src/common/types';
+import YAML from 'yaml';
 
 jest.mock('fs');
 
@@ -10,15 +12,29 @@ afterEach(() => {
 	jest.restoreAllMocks();
 });
 
-describe('Given templates are availabe in the template directory', () => {
+describe('Given templates are available in the template directory', () => {
 	beforeEach(() => {
+		jest.spyOn(fs, 'readFileSync').mockImplementation((path) => {
+			// Return the mocked file...
+			if (path.toString().includes("dummyrepo")) {
+				return Buffer.from(YAML.stringify({
+					name: "dummyrepo",
+					templates: [{
+						name: "dummytemplate",
+						path: "templates/dummtemplate"
+					}, {
+						name: "seconddummytemplate",
+						path: "templates/seconddummytemplate"
+					}]
+				} as NitricTemplateRepository))
+			}
+
+			throw new Error("ENOENT: No such file");
+		})
+
 		jest.spyOn(fs, 'readdirSync').mockReturnValue([
 			{
-				name: 'dummytemplate',
-				isDirectory: () => true,
-			} as any,
-			{
-				name: 'seconddummytemplate',
+				name: 'dummyrepo',
 				isDirectory: () => true,
 			} as any,
 		]);
@@ -26,7 +42,7 @@ describe('Given templates are availabe in the template directory', () => {
 
 	describe('When getAvailableTemplates is called', () => {
 		test('The list of templates should be returned', () => {
-			expect(getAvailableTemplates()).toEqual(['dummytemplate', 'seconddummytemplate']);
+			expect(utils.getAvailableTemplates()).toEqual(['dummyrepo/dummytemplate', 'dummyrepo/seconddummytemplate']);
 		});
 	});
 
@@ -37,7 +53,7 @@ describe('Given templates are availabe in the template directory', () => {
 			});
 
 			test('true is returned', () => {
-				expect(isTemplateAvailable('available-template')).toBe(true);
+				expect(utils.isTemplateAvailable('dummyrepo/dummytemplate')).toBe(true);
 			});
 		});
 		describe("When the template isn't available", () => {
@@ -46,7 +62,7 @@ describe('Given templates are availabe in the template directory', () => {
 			});
 
 			test('false is returned', () => {
-				expect(isTemplateAvailable('unavailable-template')).toBe(false);
+				expect(utils.isTemplateAvailable('unavailable-template')).toBe(false);
 			});
 		});
 		describe('When an error occurs while looking for the template', () => {
@@ -64,7 +80,7 @@ describe('Given no templates are available in the template directory', () => {
 
 	describe('When getAvailableTemplates is called', () => {
 		test('An empty list should be returned', () => {
-			expect(getAvailableTemplates().length).toEqual(0);
+			expect(utils.getAvailableTemplates().length).toEqual(0);
 		});
 	});
 });
@@ -76,7 +92,7 @@ describe("Given the nitric log file doesn't exist", () => {
 		});
 
 		test('the nitric log directory should be created', () => {
-			createNitricLogDir();
+			utils.createNitricLogDir();
 			expect(fs.mkdirSync).toBeCalled();
 		});
 	});
