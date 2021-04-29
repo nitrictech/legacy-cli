710528194fc2a0eb4cf21e0bc9b80efa98ab51ea
Tim Holm <tim.holm@nitric.io>
2021-04-29 15:05:00 +1000

    chore: Add copyright notices to all source files.

--- packages/common/src/utils/stack.ts
+++ packages/common/src/utils/stack.ts
@@ -1,3 +1,17 @@
+// Copyright 2021, Nitric Pty Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 import fs from 'fs';
 import path from 'path';
 import { USER_HOME } from '../paths';
@@ -7,7 +21,13 @@ import { USER_HOME } from '../paths';
  * @param file
  * @param options
  */
-export async function findFileRead(file: string, options = { encoding: 'utf-8', dir: process.cwd(), home: true }) {
+export async function findFileRead(
+	file: string,
+	options = { encoding: 'utf-8', dir: process.cwd(), home: true },
+): Promise<{
+	content: string;
+	filePath: string;
+} | null> {
 	const { dir, encoding, home } = options;
 	const dirArr = path.resolve(dir).split(path.sep);
 	const homeFilePath = path.join(USER_HOME, file);
