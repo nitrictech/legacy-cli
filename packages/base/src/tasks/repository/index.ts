abd57cea740a3d2311999a66094b7a1d0f6a9c5f
Jye Cusch <jye.cusch@nitric.io>
2021-05-07 23:25:53 +1000

    chore: cleanup make commands

--- packages/base/src/tasks/repository/index.ts
+++ packages/base/src/tasks/repository/index.ts
@@ -12,11 +12,4 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //
-
-export * from './build';
-export * from './doctor';
-export * from './make';
-export * from './repository';
-export * from './run';
-export * from './store';
-export * from './template';
+export * from './add';
