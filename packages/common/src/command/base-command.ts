0e33b5de003234921c8266d1f4135436f71c16a5
Tim Holm <tim.holm@nitric.io>
2021-05-08 14:33:24 +1000

    chore: unify @oclif/command dependencies.

--- packages/common/src/command/base-command.ts
+++ packages/common/src/command/base-command.ts
@@ -1,9 +1,36 @@
-import { Command } from "@oclif/command";
-import { AnalyticsClient } from "../analytics";
-import { Preferences } from "../preferences";
+import { Command } from '@oclif/command';
+import { AnalyticsClient } from '../analytics';
+import { Preferences } from '../preferences';
 
 export abstract class BaseCommand extends Command {
-	
+	static flags = {};
+
+
+	//public args: {
+	//	[name: string]: any;
+	//} = {};
+
+	//// NOTE: This should not be an issue
+	//public flags: T = null as any;
+
+	//async init(): Promise<void> {
+	//	// do some initialization
+	//	const { flags, args } = this.parse(this.ctor);
+	//	this.flags = flags as T;
+	//	this.args = args;
+	//}
+
+	async catch(err): Promise<any> {
+		// add any custom logic to handle errors from the command
+		// or simply return the parent class error handling
+		return super.catch(err);
+	}
+
+	async finally(err): Promise<any> {
+		// called after run and catch regardless of whether or not the command errored
+		return super.finally(err);
+	}
+
 	async run(): Promise<any> {
 		//const requiredInit = Preferences.requiresInit();
 
@@ -14,8 +41,8 @@ export abstract class BaseCommand extends Command {
 		let results: any = undefined;
 		try {
 			//if (requiredInit) {
-			console.log("Running:", this.ctor.name);
-		  //}
+			console.log('Running:', this.ctor.name);
+			//}
 			results = await this.do();
 		} catch (e) {
 			cmd.error(e, true);
