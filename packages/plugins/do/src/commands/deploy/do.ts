b4c49129be218541cee12e2a957a0a5ada3abdae
Tim Holm <tim.holm@nitric.io>
2021-06-02 15:54:47 +1000

    chore: Fix message.

--- packages/plugins/do/src/commands/deploy/do.ts
+++ packages/plugins/do/src/commands/deploy/do.ts
@@ -111,8 +111,8 @@ export default class DoDeploy extends BaseCommand {
 			});
 
 			cli.log(block`
-				If any of the above apps require config, you will need to update the managed DNS 
-				for the domains you provided with CNAME(s) that target the corresponing "Default URL"
+				If any of the above apps require DNS config, you will need to update the managed DNS 
+				for the domains you provided with CNAME(s) that target the corresponing "Default URL".
 			`);
 		} catch (error) {
 			// eat this error to avoid duplicate console output.
