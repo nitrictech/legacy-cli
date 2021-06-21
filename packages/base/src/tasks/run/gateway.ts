259af75dbeeb1b2201ce56c8e1813342375ca068
Jye Cusch <jye.cusch@nitric.io>
2021-06-21 13:33:27 +1000

    fix: runIds for gateways and api names
    also improved detection of missing entrypoint path properties

--- packages/base/src/tasks/run/gateway.ts
+++ packages/base/src/tasks/run/gateway.ts
@@ -101,7 +101,7 @@ export class RunGatewayTask extends Task<Container> {
 
 		// Build a new image for this specific API Gateway
 		const dockerOptions = {
-			name: `${stackName}-${api.name}-${runId}`,
+			name: `api-${api.name}-${runId}`,
 			// Pull the image from public docker repo
 			Image: NITRIC_BASE_API_GATEWAY_IMAGE,
 			ExposedPorts: {
@@ -119,6 +119,13 @@ export class RunGatewayTask extends Task<Container> {
 							HostPort: `${this.port}/tcp`,
 						},
 					],
+				},
+			},
+			NetworkingConfig: {
+				EndpointsConfig: {
+					[networkName]: {
+						Aliases: [`api-${api.name}`],
+					},
 				},
 			},
 		} as ContainerCreateOptions;
