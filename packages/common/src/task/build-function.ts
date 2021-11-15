8c66276b3abac75bf24f92256a643d4d627d082f
Tim Holm <tim.holm@nitric.io>
2021-11-15 14:08:32 +1100

    chore: cleanup old buildpacks integration.

--- packages/common/src/task/build-function.ts
+++ packages/common/src/task/build-function.ts
@@ -12,16 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//import execa from 'execa';
-//import path from 'path';
-//import { oneLine } from 'common-tags';
 import { Task } from './task';
 import { ContainerImage } from '../types';
 import { StackFunction } from '../stack';
-//import { DEFAULT_NITRIC_DIR, DEFAULT_BUILD_DIR } from '../constants';
-//import which from 'which';
-//import TOML from '@iarna/toml';
-//import fs from 'fs';
 import { Workspace } from '@nitric/boxygen';
 import {
 	buildGoApp,
@@ -44,15 +37,6 @@ interface BuildFunctionTaskOptions {
 	func: StackFunction;
 	provider?: string;
 }
-
-//const PACK_IMAGE = 'buildpacksio/pack:0.21.1';
-//const BUILDER_IMAGE = 'nitrictech/bp-builder-base';
-
-//const DEFAULT_PROJECT_CONFIG = {
-//	build: {
-//		exclude: [DEFAULT_NITRIC_DIR],
-//	},
-//};
 
 export class BuildFunctionTask extends Task<ContainerImage> {
 	private service: StackFunction;
@@ -121,72 +105,6 @@ export class BuildFunctionTask extends Task<ContainerImage> {
 				},
 			},
 		);
-
-		// Create a temporary default ignore file
-		// and delete it when we're done
-		//const contextDirectory = this.service.getDescriptor().context || '.';
-		//const contextBuildDirectory = `./${path.join(contextDirectory, DEFAULT_BUILD_DIR)}`;
-
-		//await fs.promises.mkdir(contextBuildDirectory, {
-		//	recursive: true,
-		//});
-		//await fs.promises.writeFile(`./${contextBuildDirectory}/${imageId}.toml`, TOML.stringify(DEFAULT_PROJECT_CONFIG));
-
-		//let baseCmd = oneLine`
-		//	build ${imageId}
-		//	--builder ${BUILDER_IMAGE}
-		//	--trust-builder
-		//	${Object.entries(this.service.getPackEnv())
-		//		.map(([k, v]) => `--env ${k}=${v}`)
-		//		.join(' ')}
-		//	-d ./.nitric/build/${imageId}.toml
-		//	--env BP_MEMBRANE_VERSION=${this.service.getVersion()}
-		//	--env BP_MEMBRANE_PROVIDER=${this.provider}
-		//	--env BP_NITRIC_SERVICE_HANDLER=${this.service.getContextRelativeDirectory()}
-		//	--pull-policy if-not-present
-		//	--default-process membrane
-		//`;
-
-		//const packInstalled = which.sync('pack', { nothrow: true });
-
-		//if (!packInstalled) {
-		//	baseCmd = oneLine`
-		//		docker run
-		//		--rm
-		//		--privileged=true
-		//		-u root
-		//		-v /var/run/docker.sock:/var/run/docker.sock
-		//		-v ${this.service.getContext()}:/workspace -w /workspace
-		//		${PACK_IMAGE} ${baseCmd}
-		//	`;
-		//} else {
-		//	baseCmd = oneLine`pack ${baseCmd} --path ${this.service.getContext()}`;
-		//}
-
-		//// Run docker
-		//// TODO: This will need to be updated for mono repo support
-		//// FIXME: Need to confirm docker sock mounting will work on windows
-		//try {
-		//	const packProcess = execa.command(baseCmd);
-
-		//	// pipe build to stdout
-		//	packProcess.stdout.on('data', (data) => {
-		//		this.update(data.toString());
-		//	});
-
-		//	// wait for the process to finalize
-		//	await packProcess;
-
-		//	// clean build files
-		//	await fs.promises.unlink(`./${contextBuildDirectory}/${imageId}.toml`);
-
-		//	// remove build directory if empty
-		//	if (fs.existsSync(contextBuildDirectory) && fs.readdirSync(contextBuildDirectory).length === 0) {
-		//		await fs.promises.rmdir(contextBuildDirectory);
-		//	}
-		//} catch (e) {
-		//	throw new Error(e.message);
-		//}
 
 		return {
 			id: imageId,
